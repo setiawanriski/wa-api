0fae53cb40cad9189217f3f1d4c422e7cabda21f
setiawanriski <49343112+setiawanriski@users.noreply.github.com>
2022-06-19 22:41:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -77,20 +77,19 @@ app.post('/sendImage', async(req,res)=>{
   }) 
 });
 
-
-// wa.create().then(client => start(client));
-wa.create({
-  sessionId: "COVID_HELPER",
-  multiDevice: true, //required to enable multiDevice support
-  authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
-  blockCrashLogs: true,
-  disableSpins: true,
-  headless: true,
-  hostNotificationLang: 'PT_BR',
-  logConsole: false,
-  popup: true,
-  qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
-}).then(client => start(client));
+wa.create().then(client => start(client));
+// wa.create({
+//   sessionId: "COVID_HELPER",
+//   multiDevice: true, //required to enable multiDevice support
+//   authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
+//   blockCrashLogs: true,
+//   disableSpins: true,
+//   headless: true,
+//   hostNotificationLang: 'PT_BR',
+//   logConsole: false,
+//   popup: true,
+//   qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
+// }).then(client => start(client));
 
 function start(client) {
   whatsappi = client;
